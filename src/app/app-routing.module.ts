036f15826f4fe6e281e7b2641ea88c37be88677a
Cesar Velasquez <ces.velasquez@hotmail.com>
2021-04-26 17:05:00 -0600

    pre loading demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NoPreloading, PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AuthComponent } from './auth/auth.component';
 import { LandingComponent } from './landing/landing.component';
 import { LayoutComponent } from './layout/layout.component';
+import { PreloadService } from './preload/preload.service';
 
 const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
       {
-        path: 'landing', component: LandingComponent, loadChildren: () => import('./landing/landing.module').then((m) => m.LandingModule),
+        path: 'landing',
+        component: LandingComponent,
+        data: { preload: true },
+        loadChildren: () => import('./landing/landing.module').then((m) => m.LandingModule), 
       },
       {
-        path: 'auth', component: AuthComponent, loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+        path: 'auth',
+        component: AuthComponent,
+        data: { preload: true },
+        loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
       },
       {
         path: 'admin', component: AdminComponent, loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
@@ -22,7 +29,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    // { preloadingStrategy: NoPreloading } default
+    { preloadingStrategy: PreloadService }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
